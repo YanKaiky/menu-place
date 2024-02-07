c4c805f3fb615db81b595cf83ae5b3f7fe2de920
Yan Kaiky <yankaikys@gmail.com>
2024-02-06 22:40:32 -0300

    feat(CategoriesCards): add margin x

--- src/components/Cards/CategoriesCards/CategoriesCards.tsx
+++ src/components/Cards/CategoriesCards/CategoriesCards.tsx
@@ -3,7 +3,7 @@ import categories from '../../../services/categories.json';
 
 export const CategoriesCards = () => {
   return (
-    <div className="max-w-[1640px] p-4">
+    <div className="max-w-[1640px] mx-auto p-4">
       <p className="font-bold text-2xl px-2 pt-4">Categories</p>
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4 py-6">
