b152a857d89caec604c0d6091edaca91eb1158c1
Yan Kaiky <yankaikys@gmail.com>
2024-02-06 23:10:58 -0300

    feat(Foods): add filters

--- src/components/Foods/Foods.tsx
+++ src/components/Foods/Foods.tsx
@@ -1,16 +1,45 @@
+import { useState } from 'react';
+import categories from '../../services/categories.json';
 import foods from '../../services/foods.json';
 import { FoodCard } from '../Cards/FoodCard/FoodCard';
 
 export const Foods = () => {
-    console.log(foods);
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+    const handleCategoryClick = (category: string) => {
+        if (selectedCategories.includes(category)) {
+            setSelectedCategories(selectedCategories.filter((c: string) => c !== category));
+        } else {
+            setSelectedCategories([...selectedCategories, category]);
+        }
+    };
+
+    const isAllSelected = selectedCategories.length === 0;
+
+    const filteredFoods = isAllSelected
+        ? foods
+        : foods.filter((food) => selectedCategories.includes(food.category));
 
     return (
         <div className='max-w-[1640px] m-auto p-4'>
             <p className="font-bold text-2xl px-2 pt-4">Filters</p>
 
+            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6 pt-4'>
+                <button className={`bg-${isAllSelected ? 'orange-500' : 'white'} text-${isAllSelected ? 'white' : 'black'} mx-2`} onClick={() => setSelectedCategories([])}>All</button>
+                {categories.map((ct, i) => (
+                    <button
+                        key={i}
+                        className={`bg-${selectedCategories.includes(ct.name) ? 'orange-500' : 'white'} text-${selectedCategories.includes(ct.name) ? 'white' : 'black'} mx-2`}
+                        onClick={() => handleCategoryClick(ct.name)}
+                    >
+                        {ct.name}
+                    </button>
+                ))}
+            </div>
+
             {/* Display foods */}
             <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
-                {foods.map((f) => (
+                {filteredFoods.map((f) => (
                     <FoodCard
                         key={f.guid}
                         image_url={f.image_url}
